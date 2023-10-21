77758fafc35f0581a7f0d4ab0bf0f0c0d6a34bcd
Funt <funt_88@mail.ru>
2023-10-21 04:21:49 +0300

    Реализация тестирования API с использованием DDT

--- Seminar_1/REST/test.py
+++ Seminar_1/REST/test.py
@@ -14,7 +14,7 @@ def test_step1(login):
     res_list = res.get("data")
     if res_list is None:
         raise Exception("Результат не содержит данных")
-    res_id_list = [item['id'] for item in res_list]
+    res_id_list = [item["id"] for item in res_list]
     assert 1234 in res_id_list, "тест не пройден"
 
 # ДЗ-1. Добавить в задание с REST API ещё один тест, в котором создаётся новый пост,
@@ -24,9 +24,9 @@ def test_step1(login):
 def test_check_post_create(create_post, login):
     create_post
     token = login
-    result = requests.Session().get(url=data['url_post'], headers={'X-Auth-Token': token}).json()['data']
-    list_description = [i['description'] for i in result]
-    assert data['description'] in list_description, 'check_post_create FAIL'
+    result = requests.Session().get(url=data.get("url_post"), headers={"X-Auth-Token": token}).json()["data"]
+    list_description = [i["description"] for i in result]
+    assert data["description"] in list_description, "check_post_create FAIL"
 
 
 if __name__ == '__main__':
